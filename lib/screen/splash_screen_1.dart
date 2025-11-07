c46b48e5b68459139031f8c97a6e1ad3f14a08fb
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-11-07 20:34:11 +0700

    Project Flutter UTS Update Splash Screen

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -10,6 +10,20 @@ class MySplashscreen1 extends StatefulWidget {
 class _MySplasscreen1State extends State<MySplashscreen1> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 30),
+          Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.blueGrey,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
